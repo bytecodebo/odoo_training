ac60c006a9ef2c6f1f97ffb51567b48d520093ec
Douglas Aparicio Bermudez <bermudezdouglas@gmail.com>
2025-07-11 20:01:05 -0400

    examen Douglas Aparicio

--- x-evaluacion/douglasAparicioBermudez/trn_examen_ejercicio/models/res_partner.py
+++ x-evaluacion/douglasAparicioBermudez/trn_examen_ejercicio/models/res_partner.py
@@ -1,7 +1,11 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    #para implementar la restriccion de un cliente padre no puede tener como hijo a otro cliente padre
+    #entonces añadiremos un campo booleano, por ejemplo is_parent_customer.
+    is_parent_customer = fields.Boolean(string='¿Es Cliente Padre?')
 
     child_code = fields.Integer(
         string='Código correlativo',
@@ -18,3 +22,13 @@ class ResPartner(models.Model):
             max_code = max(parent.child_ids.mapped('child_code') or [0])
             vals['child_code'] = max_code + 1
         return super(ResPartner, self).create(vals)
+
+    #aqui comprobamos que un cliente padre no puede tener como hijo a otro cliente padre
+    # al intentar asignar como padre a otro cliente que también es “cliente padre”, arrojará un error.
+    @api.constrains('parent_id', 'is_parent_customer')
+    def _check_parent_customer_constraint(self):
+        for rec in self:
+            if rec.parent_id and rec.parent_id.is_parent_customer and rec.is_parent_customer:
+                raise ValidationError(
+                    'Un cliente padre no puede tener como hijo a otro cliente padre.'
+                )
